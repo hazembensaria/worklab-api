1ac2fd01f0f9a7c6b783b3cebabb7dce0859b866
ben saria hazem <bensariahazem@gmail.com>
2022-11-27 21:38:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+const  Connection  = require('./config/DBConnection');
+
+Connection();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+//----------------------------setHeaders------------------------------------------------
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin , X-Requested-With , Content-Type , Accept,authorization");//authorization to store the token
+    res.header("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    next();
+})
+
+
+app.get("/",(req ,res)=>{
+    res.send('you are connected')
+    console.log("helo")})
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+const app=require("./app");
+
+const http=require("http");
+
+dotenv.config()
+
+const PORT =  4000;
+app.set("PORT",PORT)
+const server=http.createServer(app)
+server.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}...`))
