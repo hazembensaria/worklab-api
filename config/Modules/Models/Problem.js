fb30592bb6c929c1aeeeaa84d31e0b8797fdc492
amine_tt <mohamedaminetouihri9@gmail.com>
2023-01-14 18:20:33 +0100

    add solve it page

--- config/Modules/Models/Problem.js
+++ config/Modules/Models/Problem.js
@@ -5,7 +5,9 @@ const UniqueValidator=require("mongoose-unique-validator");
 const problem=mongoose.Schema({
     description:{type:String,require:true},
     name:{type:String,require:true,unique:true},
-    difficulty:{type:String,require:true}
+    difficulty:{type:String,require:true},
+    category:{type:String,require:true},
+    comments:{type:Array}
 })
 problem.plugin(UniqueValidator);
 module.exports=mongoose.model("Problem",problem)
